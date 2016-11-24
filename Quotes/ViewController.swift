7468c7af40bf81ba85cfa981185cc4f6bb43a94d
Roman Tsopin <rotsopin@avito.ru>
2016-11-24 21:21:28 +0300

    Initial Commit

---
+++ Quotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quotes
+//
+//  Created by Цопин Роман on 24/11/2016.
+//  Copyright © 2016 Цопин Роман. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
